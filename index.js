f80895aaf818aca47de5df9a045d4d78173253a2
Diachenko Maksym <maxdiach137@gmail.com>
2021-11-04 17:09:01 +0200

    Added Ewok encoding on localhost.

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
+const { default: axios } = require('axios');
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-console.log("Ewok encode tests");
-console.log(ewokEncode("you can do it"));
-console.log(ewokEncode("R2-D2"));
-
-
-// Create Express Server
-const app = express();
+var url = require('url');
 
 // Configuration
 const PORT = 3033;
 const HOST = "localhost";
 const API_SERVICE_URL = "https://swapi.dev/api";
 
+// Create Express Server
+const app = express();
+
 app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+    console.log(`Starting SWAPI Proxy at ${HOST}:${PORT}`);
 });
 
-app.use('/', createProxyMiddleware({
-    target: API_SERVICE_URL,
-    changeOrigin: true,
-    pathRewrite: {
-        [`^/`]: '',
-    },
- }));
+app.use('/', function (req, res) {
+    var url_parts = url.parse(req.url, true);
 
+    var query_str = url_parts.search;
+    if (query_str === null) query_str = "";
 
-function ewokEncode(text){
-  return text.toLowerCase().replace(/[aeiouy]+/g,"i").replace(/((?![aeiou])[a-z])+/g,"b");
+    axios({
+        "method": req.method,
+        "url": API_SERVICE_URL + req.path + query_str, res
+    }).then((response) => {
+        if (req.query["encoding"] == "ewok") {
+            for (var key in response.data) {
+                if (Array.isArray(response.data[key])) {
+                    response.data[key] = response.data[key].map(ewokEncode);
+                } else if (typeof response.data[key] === 'string') {
+                    response.data[key] = ewokEncode(response.data[key]);
+                }
+            }
+            console.log("Response with custom Ewok encoding sent!");
+            res.send(response.data);
+        } else {
+            console.log("Response sent!");
+            res.send(response.data);
+        }
+    }).catch(function (err) {
+        res.send(err.message);
+    });
+});
+
+function ewokEncode(text) {
+    return text.toLowerCase().replace(/[aeiouy]/g, "i").replace(/((?![aeiouy])[a-z])/g, "b");
 }
